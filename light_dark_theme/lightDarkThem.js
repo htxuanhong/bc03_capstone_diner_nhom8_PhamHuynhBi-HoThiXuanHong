f718db20ed71167c5e45cb28f2ebb5f9ac24d3aa
htxuanhong <hoxuanhong1998@gmail.com>
2022-05-04 00:01:18 +0700

    sua ld

--- light_dark_theme/lightDarkThem.js
+++ light_dark_theme/lightDarkThem.js
@@ -1,3 +1,9 @@
 document.getElementById("switchButton").onclick = function () {
   document.getElementById("myBody").classList.toggle("dark");
+
+  var lightDarks = document.getElementsByClassName("light-dark");
+
+  for (var i = 0; i < lightDarks.length; i++) {
+    lightDarks.item(i).classList.toggle("dark");
+  }
 };
